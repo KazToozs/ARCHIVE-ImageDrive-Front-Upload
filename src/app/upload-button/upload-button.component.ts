678cbcd340e1ca56ee844347acf8a760bc961666
Will Toozs <cris.hobson@hotmail.com>
2020-11-19 10:22:48 +0100

    Set UI response messages for status returns

--- src/app/upload-button/upload-button.component.ts
+++ src/app/upload-button/upload-button.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { HttpEventType, HttpErrorResponse } from '@angular/common/http';
-import { of } from 'rxjs';
+import { BehaviorSubject, Observable, of } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
 import { UploadService } from 'src/services/upload.service';
 
@@ -13,16 +13,25 @@ type FileToUpload = { data: File, inProgress: boolean, progress: number };
   styleUrls: ['./upload-button.component.scss']
 })
 export class UploadButtonComponent implements OnInit {
-  description = '';
+  private UIMessageSubject: BehaviorSubject<string> = new BehaviorSubject('');
 
+  public readonly UIMessage: Observable<string> = this.UIMessageSubject.asObservable();
+
+  description = '';
   fileToUpload: FileToUpload; // TODO not ideal?
 
-  constructor(private uploadService: UploadService) { }
+  constructor(private uploadService: UploadService) {
+  }
 
   ngOnInit(): void {
   }
 
   uploadFile(file: FileToUpload) {
+    if (!this.description) {
+      console.log('blahallaje')
+      this.UIMessageSubject.next('Please set a description');
+      return;
+    }
     // TODO remove
     console.log('Filename: ' + file.data.name);
     console.log('Type: ' + file.data.type);
@@ -35,6 +44,7 @@ export class UploadButtonComponent implements OnInit {
     file.inProgress = true;
     this.uploadService.upload(formData).pipe(
       map(event => {
+        this.setUIMessage('Uploading...');
         switch (event.type) {
           case HttpEventType.UploadProgress:
             file.progress = Math.round(event.loaded * 100 / event.total);
@@ -44,14 +54,19 @@ export class UploadButtonComponent implements OnInit {
         }
       }),
       catchError((error: HttpErrorResponse) => {
+        this.setUIMessage(`An error occured with the upload: ${error.status}: ${error.error}`);
         file.inProgress = false;
-        return of(`${file.data.name} upload failed.`);
+        return of(`${file.data.name} upload failed: ${error}`);
       })).subscribe((event: any) => {
         if (typeof (event) === 'object') {
+          console.log(event)
+          this.setUIMessage('Upload succeeded');
+          // TODO delete this
           console.log('Server returned: ' + event.body);
         }
       });
   }
+
 
   onFileChanged(event) {
     this.fileToUpload = null;
@@ -60,13 +75,17 @@ export class UploadButtonComponent implements OnInit {
       if ((file.type === 'image/jpeg' || file.type === 'image/png')
         && file.size <= 500000) {
         // confirm file recieved
+        this.setUIMessage('File ready to upload');
         this.fileToUpload = { data: file, inProgress: false, progress: 0 };
       }
       else {
         // TODO state error in size or file type
+        this.setUIMessage('Please upload a JPEG/PNG below 500Kb');
       }
     }
   }
 
-
+  setUIMessage(str: string) {
+    this.UIMessageSubject.next(str);
+  }
 }
